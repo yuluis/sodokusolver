37f3a92d58d751bec9feb1fdbb94b522263a12a1
Luis Yu <yuluis@gmail.com>
2017-10-29 14:20:55 -0700

    Add files via upload
    fix but checking duplicate within block of 3x3 due to error in range function interpretation (does not include end point)
    improve formatting of output using numpy

--- sodokusolver.py
+++ sodokusolver.py
@@ -25,12 +25,12 @@ def isValid(m, aTry):
     # check entry is not duplicated within the 9 cell square
     xcell = (xTry / 3) * 3
     ycell = (yTry / 3) * 3
-    for ii in range(0, 2):
-        for jj in range(0, 2):
+    for ii in range(0, 3):
+        for jj in range(0, 3):
             #print "debug", xcell+ii, ycell+jj, xTry, ii, yTry, jj
+            #print (xcell, ycell, ii, jj, val)
             if val == m[xcell+ii][ycell+jj] :
                 return -4, "duplicate in cell", aTry, ii, jj
-
 
     return 1 # must be valid
 
@@ -71,17 +71,19 @@ def countzeros(x,y,m):
 # for each non-zero entry, calculate the which cell is the most constrained by non-zero row and column entries
 # the least number of zero contraints will the the next time returned
 # if no zero entries can be found 0,0,0 is returned
-def getNextOrdered(m):
+# select cells random proportional to how contrained a cell is
+def getNextConstrained(m):
     l = len(m[0])
     minzeros = 99
     for i in range (l) :
         for j in range (l) :
             if 0 == m[i][j] :
-                zeros = countzeros(i,j,m)
-                if zeros < minzeros :
-                    xmin = i
-                    ymin = j
-                    minzeros = zeros
+                if random.randint(1,10) < 4 : # don't always pick the cell to work on to add randomness to the cell selection
+                    zeros = countzeros(i,j,m)
+                    if zeros < minzeros :
+                        xmin = i
+                        ymin = j
+                        minzeros = zeros
 
     if 99 == minzeros:
         return 0,0,0 # no zeros found
@@ -96,19 +98,18 @@ def getNextOrdered(m):
 #       if valid then recursively call function again on new matrix
 #   if all entries in matrix have been filled we must have found a solution; print solution
 def solve(m, depth) :
-    r,x,y = getNextOrdered(m)
+    r,x,y = getNextConstrained(m)
     #print "solve function called", r, x, y
     if 0 == r :
         return "solved!", m
 
-    for i in range(40):
+    for i in range(40): # warning,
         rv = random.randint(1,9)
         o1 = isValid(m, [x, y, rv])
         if 1 == o1 :
             m[x][y] = rv
             #print "new sodoku entry", x, y, rv, mtmp
             solve(m, depth+1)
-
     return m
 
 # calculate the number of zeros in the matrix (lower the better; 0 means puzzle solved
@@ -180,6 +181,7 @@ p8 = [0,4,0,0,0,0,0,0,7]
 p9 = [0,0,7,0,0,0,3,0,0]
 pe = [p1,p2,p3,p4,p5,p6,p7,p8,p9]
 
+# 2012 finnish mathematician Arto Inkala claimed worlds hardest sodoku puzzle
 p1 = [8,0,0,0,0,0,0,0,0]
 p2 = [0,0,7,5,0,0,0,0,9]
 p3 = [0,3,0,0,0,0,1,8,0]
@@ -193,41 +195,47 @@ pf = [p1,p2,p3,p4,p5,p6,p7,p8,p9]
 start_time = time.time()
 
 def sodokusolver (p) :
-    print ("begin new solve from top level",p)
+    print ("New puzzle, non-zero count",np.count_nonzero(p))
+    pizza = np.array(p)
+    print(pizza)
+
     bestGrade = 81
-    for i in range (1000) :
+    random.seed(time.time())
+    attempts = 8000
+    for i in range (attempts) : # number of attempts to solve
+        if 0 == i % 1000 :
+            print("."),
         random.seed(i)
         puzzlecopy = copy.deepcopy(p)
         puzzlecopy = solve(puzzlecopy, 0)
         myGrade = grade(puzzlecopy)
         if 0 ==myGrade :
-            print("solved (tries)?", i)
-            print("time elapsed", time.time() - start_time, "grade", myGrade, puzzlecopy)
-            pizza = np.array(p)
-            print(pizza)
+            print("solved (tries)", i, "time elapsed", time.time() - start_time, "grade", myGrade, "nonzeros",np.count_nonzero(puzzlecopy))
             pizza = np.array(puzzlecopy)
             print(pizza)
-
             return i
         if myGrade < bestGrade  :
             bestGrade = min(myGrade, bestGrade)
             bestSolution = copy.deepcopy(puzzlecopy)
             #print("time elapsed", time.time()-start_time,"grade", myGrade, puzzlecopy)
-    print("can't solve! myBestGrade is", bestGrade, bestSolution)
+    print("Can't solve attempts", attempts, "myBestGrade", bestGrade)
+    pizza = np.array(bestSolution)
+    print(pizza)
     return -999
 
-#sodokusolver(pa)
-#sodokusolver(pb)
-#sodokusolver(pc)
-#sodokusolver(pd)
-#sodokusolver(pf)
+sodokusolver(pa)
+sodokusolver(pb)
+sodokusolver(pc)
+sodokusolver(pd)
+sodokusolver(pe)
+sodokusolver(pf)
 
 # returns a valid sodoku puzzle of random number of entries from 1 to 80
 def sodokurandom(initpuzzle) :
     m = copy.deepcopy(initpuzzle)
-    #entrytarget = random.randint(30, 50)
+    #entrytarget = random.randint(40)
     #print entrytarget
-    for i in range (40) :
+    for i in range (40,60) :
         x = random.randint(0,8)
         y = random.randint(0,8)
         z = random.randint(1,9)
@@ -235,9 +243,6 @@ def sodokurandom(initpuzzle) :
         if 1 == isValid(m, [x,y,z]):
             m[x][y] = z
     return (m)
-
-
-
 
 jmax = 0
 jmaxproblem = copy.deepcopy(pc)
@@ -247,8 +252,7 @@ for i in range (40) :
     if jmax < j :
         jmax = j
         jmaxproblem = copy.deepcopy(pz)
-
-print("most difficult problem", jmax, jmaxproblem)
+print("most difficult random problem", jmax, jmaxproblem)
 
 
 
